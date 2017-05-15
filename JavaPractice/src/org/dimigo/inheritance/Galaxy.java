dd8f4c6959e38c773f1f566805f422a762807cba
yoojin <yoojin@yoojin>
2017-05-15 09:42:40 +0900

    LG

--- JavaPractice/src/org/dimigo/inheritance/Galaxy.java
+++ JavaPractice/src/org/dimigo/inheritance/Galaxy.java
@@ -10,22 +10,25 @@ package org.dimigo.inheritance;
  *
  * 1. 개요 :
  * 2. 작성일 : 2017. 5. 12.
- *</pre>
+ * </pre>
  *
  * @author : yoojin
  * @version : 1.0
  */
 public class Galaxy extends SmartPhone {
-	public Galaxy(){
+	public Galaxy() {
 		super();
 	}
-	public Galaxy(String model, String company, int price){
-		super(model,company,price);
+
+	public Galaxy(String model, String company, int price) {
+		super(model, company, price);
 	}
-	public void pay(){
+
+	public void pay() {
 		System.out.println("삼성 페이로 결제합니다.");
 	}
-	public void useWirelessCharging(){
+
+	public void useWirelessCharging() {
 		System.out.println("무선 충전 기능을 이용합니다.");
 	}
 }
--- JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhone.java
@@ -10,7 +10,7 @@ package org.dimigo.inheritance;
  *
  * 1. 개요 :
  * 2. 작성일 : 2017. 5. 12.
- *</pre>
+ * </pre>
  *
  * @author : yoojin
  * @version : 1.0
@@ -19,35 +19,40 @@ public class SmartPhone {
 	private String model;
 	private String company;
 	private int price;
-	public SmartPhone(){
-		
+
+	public SmartPhone() {
+
 	}
-	public SmartPhone(String model, String company, int price){
-		this.model=model;
-		this.company=company;
-		this.price=price;
+
+	public SmartPhone(String model, String company, int price) {
+		this.model = model;
+		this.company = company;
+		this.price = price;
 	}
-	public void turnOn(){
-		System.out.println(model+"의 전원을 켭니다.");
+
+	public void turnOn() {
+		System.out.println(model + "의 전원을 켭니다.");
 	}
-	public void turnOff(){
-		System.out.println(model+"의 전원을 끕니다.");
+
+	public void turnOff() {
+		System.out.println(model + "의 전원을 끕니다.");
 	}
-	public void pay(){
-		
+
+	public void pay() {
+
 	}
-	public void useSpecialFunction(SmartPhone phone){
-		if(phone instanceof IPhone){
-			((IPhone)phone).useAirDrop();
+
+	public void useSpecialFunction(SmartPhone phone) {
+		if (phone instanceof IPhone) {
+			((IPhone) phone).useAirDrop();
+		} else if (phone instanceof Galaxy) {
+			((Galaxy) phone).useWirelessCharging();
 		}
-		else if(phone instanceof Galaxy){
-			((Galaxy)phone).useWirelessCharging();
-		}
 	}
+
 	public String toString() {
-		return "모델명 : "+ model +", 제조사 : "+company+", 가격 : "+String.format("%,d", price)+"원";
-		
+		return "모델명 : " + model + ", 제조사 : " + company + ", 가격 : " + String.format("%,d", price) + "원";
+
 	}
-	
 
 }
