af56625f1b7dc92fe64510c6d3de328ab3d25672
yoojin <yoojin@yoojin>
2017-06-15 15:38:10 +0900

    arako

---
+++ JavaPractice/src/org/dimigo/io/ArakoMenu.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package org.dimigo.io;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.Writer;
+
+/**
+ * <pre>
+ * org.dimigo.io
+ *   |_ ArakoMenu
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 6. 15.
+ * </pre>
+ *
+ * @author : yoojin
+ * @version : 1.0
+ */
+public class ArakoMenu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				Writer writer = new BufferedWriter(new FileWriter("files/menu.txt"));
+				BufferedReader reader = new BufferedReader(new FileReader("files/menu.txt"))) {
+			String str;
+			while ((str = br.readLine()) != null) {
+				writer.write(str);
+				writer.write("\n");
+				writer.flush();
+			}
+			System.out.println("<< 메뉴 출력 >>");
+			String str1;
+			while ((str1 = reader.readLine()) != null) {
+				System.out.println(str1);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
